cb381f29e6612ab21eb830f6bb3a8132914014e6
DiegoIacabo <diego.iacabo@sptech.school>
2026-09-05 23:45:08 -0300

    Adicionando CrossOrigin nas Controllers

--- backend/src/main/java/individual/backend/GeneroController.java
+++ backend/src/main/java/individual/backend/GeneroController.java
@@ -3,12 +3,14 @@ package individual.backend;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/generos")
 public class GeneroController {
